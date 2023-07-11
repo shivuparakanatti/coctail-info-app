546b312ac52005b60974430191a9c9cf36463e3a
shivuparakanatti <103122119+shivuparakanatti@users.noreply.github.com>
2023-07-11 21:18:30 +0530

    Update

--- src/components/CoctailContainer.js
+++ src/components/CoctailContainer.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom"
 const CoctailContainer =(props)=>{
     return (
         <div>
-            
+        
         <div className="ItemContainer">
                 <input type={'search'} placeholder='Search for your coctail' />
                 <h1>Coctails</h1>
